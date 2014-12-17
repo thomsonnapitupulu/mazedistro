ca9feebba93e2984d81f10abc6763db518ab55aa
thomsonnapitupulu <thomson.p.napitupulu@gmail.com>
2014-12-17 13:09:48 +0700

    first commit
    opencart source

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/mazedistro/admin/');
+define('HTTP_CATALOG', 'http://localhost:81/mazedistro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/mazedistro/admin/');
+define('HTTPS_CATALOG', 'http://localhost:81/mazedistro/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\mazedistro/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\mazedistro/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\mazedistro/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\mazedistro/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\mazedistro/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\mazedistro/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\mazedistro/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\mazedistro/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\mazedistro/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\mazedistro/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\mazedistro/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\mazedistro/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mazedistrodb');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/mazedistro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/mazedistro/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\mazedistro/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\mazedistro/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\mazedistro/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\mazedistro/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\mazedistro/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\mazedistro/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\mazedistro/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\mazedistro/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\mazedistro/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\mazedistro/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\mazedistro/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mazedistrodb');
+define('DB_PREFIX', 'oc_');
